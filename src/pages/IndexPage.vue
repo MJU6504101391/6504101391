05a46ad8cc032a135d1c3e74dd93e34ea9752be2
sakkarin <nightkeezaza@gmail.com>
2026-08-21 14:31:22 +0700

    v.6-Button with Japanese language-391

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,12 +1,12 @@
-<template>
+﻿<template>
   <q-page class="q-pa-md">
     <q-form class="q-gutter-md" style="max-width: 600px" @submit.prevent="onSubmit" @reset="onReset">
       <q-input v-model="name" filled label="Your name *" hint="Name and surname" />
       <q-input v-model="age" filled type="number" label="Your age *" />
       <q-toggle v-model="accept" label="I accept the license and terms" />
       <div>
-        <q-btn label="SUBMIT" type="submit" color="primary" />
-        <q-btn label="RESET" type="reset" color="primary" flat class="q-ml-sm" />
+        <q-btn label="SUBMIT (提出する)" type="submit" color="primary" />
+        <q-btn label="RESET (リセット)" type="reset" color="primary" flat class="q-ml-sm" />
       </div>
     </q-form>
   </q-page>
@@ -41,3 +41,4 @@ function onReset () {
   accept.value = false
 }
 </script>
+
