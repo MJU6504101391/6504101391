db04aaddb3febc785aa42acf6cdf1bdfcef499e9
sakkarin <nightkeezaza@gmail.com>
2026-08-21 14:32:45 +0700

    Resolve final multilingual form-391

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,9 @@
 ﻿<template>
   <q-page class="q-pa-md">
     <q-form class="q-gutter-md" style="max-width: 600px" @submit.prevent="onSubmit" @reset="onReset">
-<<<<<<< HEAD
       <q-input v-model="name" filled label="Your name (ชื่อ-สกุล) *" hint="Name and surname" />
       <q-input v-model="age" filled type="number" label="Your age (อายุ) *" />
-      <q-toggle v-model="accept" label="I accept the license and terms" />
-=======
-      <q-input v-model="name" filled label="Your name *" hint="Name and surname" />
-      <q-input v-model="age" filled type="number" label="Your age *" />
       <q-toggle v-model="accept" label="I accept the license and terms (我接受许可和条款语言)" />
->>>>>>> Chinese-language
       <div>
         <q-btn label="SUBMIT (提出する)" type="submit" color="primary" />
         <q-btn label="RESET (リセット)" type="reset" color="primary" flat class="q-ml-sm" />
